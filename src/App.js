d18638f12bf5ba4952548c3b07852d45d3342add
Yash Vinod Jadhav <jadhavyash030@gmail.com>
2023-08-22 18:59:52 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -175,15 +175,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import About from './components/About';
+import About from './components/About';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-// import { Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 
  
 function App() {
@@ -199,13 +199,21 @@ function App() {
           setAlert(null);
       }, 1500);
   }
+  // const relmoveBodyClasses=()=>{
+  //   document.body.classList.remove('bg-light')
+  //   document.body.classList.remove('bg-dark')
+  //   document.body.classList.remove('bg-warning')
+  //   document.body.classList.remove('bg-danger')
+  //   document.body.classList.remove('bg-success')
+  // }
 
   const toggleMode = ()=>{
+    // document.body.classList.add('bg-'+cls)
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled", "success");
-      document.title = 'TextUtils - Dark Mode';
+      // document.title = 'TextUtils - Dark Mode';
       // setInterval(() => {
       //   document.title = 'TextUtils is Amazing Mode';
       // }, 2000);
@@ -217,28 +225,28 @@ function App() {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
-      document.title = 'TextUtils - Light Mode';
+      // document.title = 'TextUtils - Light Mode';
     }
   }
   return (
     <>
     {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
     {/* <Navbar/> */}
-    {/* <Router> */}
+    <Router>
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
     <Alert alert={alert}/>
     <div className="container">
-    {/* <Routes> */}
+    <Routes>
     {/* /users --> Component 1
         /users/home --> Component 2 */}
-          {/* <Route exact path="/about" element={<About />}> */}
-          {/* </Route> */}
-          {/* <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>}> */}
-          <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
-          {/* </Route> */}
-    {/* </Routes> */}
+          <Route exact path="/about" element={<About mode={mode} />}>
+          </Route>
+          <Route exact path="/" element={<TextForm showAlert={showAlert} heading=" Try TextUtils - Word Counter, Character Counter,Remove extra Spaces" mode={mode}/>}>
+          {/* <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/> */}
+          </Route>
+    </Routes>
     </div>
-    {/* </Router> */}
+    </Router>
     </> 
   );
 }
